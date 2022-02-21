283a854a36d8b871936a8b2f3a48b9e11c165fcd
Strada Fabio <fabio.strada@allitude.it>
2022-02-21 15:35:43 +0100

    update shared module

--- projects/shared/src/lib/shared.service.ts
+++ projects/shared/src/lib/shared.service.ts
@@ -2,10 +2,6 @@ import { Injectable, Optional, SkipSelf } from '@angular/core';
 import { SharedModule } from './shared.module';
 
 @Injectable({
-  providedIn: 'root'
-})
-
-@Injectable({
   deps: [[new Optional(), new SkipSelf(), SharedService]],
   providedIn: SharedModule,
   useFactory: (instance: SharedService | null) => instance || new SharedService(),
